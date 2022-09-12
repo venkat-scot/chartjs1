2cf71b11ea183ae162762534076b02ee58d5cd8e
venkat-scot <profession.course@gmail.com>
2022-09-12 16:04:19 +0100

    Updates

---
+++ precache-manifest.a2ffb55ec2760998df8c54868cd8e809.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc68d0c60facb3422bf4",
+    "url": "/chartjs1/static/css/main.a8e6a998.chunk.css"
+  },
+  {
+    "revision": "bc68d0c60facb3422bf4",
+    "url": "/chartjs1/static/js/main.bc68d0c6.chunk.js"
+  },
+  {
+    "revision": "3b47e211118505985a05",
+    "url": "/chartjs1/static/js/1.3b47e211.chunk.js"
+  },
+  {
+    "revision": "694039d06e92c5c29222",
+    "url": "/chartjs1/static/js/runtime~main.694039d0.js"
+  },
+  {
+    "revision": "0e76338eef80995d4210a303c6d4614e",
+    "url": "/chartjs1/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/chartjs1/precache-manifest.a2ffb55ec2760998df8c54868cd8e809.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/chartjs1/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
